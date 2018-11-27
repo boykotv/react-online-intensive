2e6b85eace1930571742c6e18fdcce1d1b36c799
Boyko Tetiana <tanya.titanya@gmail.com>
2018-11-27 23:55:17 +0200

    add Delete

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -11,7 +11,6 @@ import Spinner from 'components/Spinner';
 //Instruments
 import Styles from './styles.m.css';
 import {getUniqueID, delay} from 'instruments';
-import { deflate } from 'zlib';
 
 export default class Feed extends Component {
     constructor() {
@@ -20,6 +19,7 @@ export default class Feed extends Component {
         this._createPost = this._createPost.bind(this);
         this._setPostsFetchingState = this._setPostsFetchingState.bind(this);        
         this._likePost = this._likePost.bind(this);
+        this._removePost = this._removePost.bind(this);        
     }
 
     state = {
@@ -32,6 +32,12 @@ export default class Feed extends Component {
             }, 
             { 
                 id: '1234', 
+                comment: 'Hiiiiiiiiiiiiiiii!', 
+                created: 1526825076855,
+                likes: [],
+            },
+            { 
+                id: '12345', 
                 comment: 'Hiiiiii!', 
                 created: 1526825076855,
                 likes: [],
@@ -93,10 +99,44 @@ export default class Feed extends Component {
         })
     }
 
+    async _removePost (id) {
+        const {currentUserFirstName, currentUserLastName} = this.props;
+        this._setPostsFetchingState(true);
+
+        console.log('удалить пост с этим id', id);
+
+        await delay(1200);
+
+        const newPosts = this.state.posts.map((post) => {
+            if (post.id !== id) {
+
+                console.log('оставить этот пост в массиве', post.id);
+
+                return {
+                    ...post,
+                    likes: [
+                        {
+                            id:         getUniqueID(),
+                            firstName:  currentUserFirstName,
+                            lastName:   currentUserLastName,
+                        }
+                    ],
+                };
+            }        
+               return post;
+        });
+
+        console.log('newPosts', newPosts);
+        this.setState({
+            posts: newPosts,
+            isPostFetching: false,
+        })
+    }
+
     render() {
         const { posts, isPostFetching } = this.state;
         const postsJSX = posts.map((post) => {
-            return <Post key = { post.id } {...post} _likePost = { this._likePost } />;
+            return <Post key = { post.id } {...post} _likePost = { this._likePost } _removePost = { this._removePost } />;
         });
 
         return (
