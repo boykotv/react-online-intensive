e8f22ae9ddcf02426d8675d65acdd0e4386b2692
Boyko Tetiana <tanya.titanya@gmail.com>
2018-12-02 18:57:08 +0200

    lesson 7 - refactoring

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 //Components
+import { withProfile } from 'components/HOC/withProfile';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
@@ -12,16 +13,8 @@ import Spinner from 'components/Spinner';
 import Styles from './styles.m.css';
 import {getUniqueID, delay} from 'instruments';
 
+@withProfile
 export default class Feed extends Component {
-    constructor() {
-        super();
-
-        this._createPost = this._createPost.bind(this);
-        this._setPostsFetchingState = this._setPostsFetchingState.bind(this);        
-        this._likePost = this._likePost.bind(this);
-        this._removePost = this._removePost.bind(this);        
-    }
-
     state = {
         posts: [
             { 
@@ -46,13 +39,13 @@ export default class Feed extends Component {
         isPostFetching: false,
     }
 
-    _setPostsFetchingState (state) {
+    _setPostsFetchingState = (state) => {
         this.setState({
             isPostFetching: state,
         })
     }
 
-    async _createPost (comment) {
+    _createPost = async (comment) => {
         this._setPostsFetchingState(true);
 
         const post = {
@@ -70,7 +63,7 @@ export default class Feed extends Component {
         }));
     }
 
-    async _likePost (id) {
+    _likePost = async (id) => {
         const { currentUserFirstName, currentUserLastName } = this.props;
         this._setPostsFetchingState(true);
 
@@ -99,7 +92,7 @@ export default class Feed extends Component {
         })
     }
 
-    async _removePost (id) {
+    _removePost = async (id) => {
         const { posts } = this.state;
         this._setPostsFetchingState(true);
 
