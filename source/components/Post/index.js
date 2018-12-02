11b75777ab60e8aecfdb7a933855c5e770850782
Boyko Tetiana <tanya.titanya@gmail.com>
2018-12-02 21:08:00 +0200

    lesson 8 - add Socket

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -27,15 +27,39 @@ export default class Post extends Component {
         _removePost(id);
     }
 
+    _getCross = () => {
+        const { 
+            firstName, 
+            lastName, 
+            currentUserFirstName, 
+            currentUserLastName 
+        } = this.props;
+
+        return `${firstName} ${lastName}` === 
+            `${currentUserFirstName} ${currentUserLastName}` ? (
+                <span className = { Styles.cross } onClick = { this._removePost } /> 
+            ) : null;
+    };
+
     render() {
-        const { comment, created, _likePost, id, likes } = this.props; 
-        const { avatar, currentUserFirstName, currentUserLastName } = this.props;
+        const { 
+            comment, 
+            created, 
+            _likePost, 
+            id, 
+            likes, 
+            avatar, 
+            firstName, 
+            lastName 
+        } = this.props;
+
+        const cross = this._getCross();
 
         return (           
             <section className = { Styles.post }>
-                <span className = { Styles.cross } onClick = { this._removePost } id = { id }/>
+                { cross }
                 <img src = { avatar } />
-                <a>{ `${ currentUserFirstName } ${ currentUserLastName }` }</a>
+                <a>{ `${ firstName } ${ lastName }` }</a>
                 <time>{ moment.unix(created).format('MMMM D h:mm:ss a') }</time>
                 <p>{ comment }</p>
                 <Like _likePost = { _likePost } id = { id } likes = { likes } />
