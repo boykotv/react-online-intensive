d4a9ea787e8e9b5d014e04ecfad382bef651380a
Tatiana Boyko <tanya.titanya@gmail.com>
2018-12-18 00:40:43 +0200

    tests for Composer

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -8,10 +8,12 @@ import { withProfile } from 'components/HOC/withProfile';
 //Instruments
 import Styles from './styles.m.css';
 
-@withProfile
-export default class Composer extends Component {
+//@withProfile
+export class Composer extends Component {
     static propTypes = {
         _createPost: PropTypes.func.isRequired,
+        avatar: PropTypes.string.isRequired,
+        currentUserFirstName : PropTypes.string.isRequired,
     };
 
     state = {
@@ -72,3 +74,4 @@ export default class Composer extends Component {
         );
     }
 }
+export default withProfile(Composer);
