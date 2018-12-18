7bba6bde4513c8698a1e8d1ef5f2ed939741b5a9
boykotv <tanya.titanya@gmail.com>
2018-12-18 21:04:09 +0200

    add test

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -33,6 +33,7 @@ describe('instruments:', () => {
 
     test('getUniqueID function should produse a string of a desired given length', () => {
         expect(typeof getUniqueID()).toBe('string');
+        expect (getUniqueID()).toHaveLength(15);
         expect (getUniqueID(5)).toHaveLength(5);
         expect (getUniqueID(13)).toHaveLength(13);
     });
